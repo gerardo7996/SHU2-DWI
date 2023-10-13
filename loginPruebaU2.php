d69c9fb6aabeee2c11048eeb95e0f92b9a4a3351
alejandramoralestorres16 <126217488+alejandramoralestorres16@users.noreply.github.com>
2023-10-13 12:50:53 -0600

    Add files via upload

---
+++ loginPruebaU2.php
@@ -0,0 +1,118 @@
+
+<html>
+	<head>
+		<title>Inicio de sesion</title>
+		<!-- CDN  MDB 4.19.1 -->
+
+<!-- CSS -->
+<link rel="icon" href="https://images.vexels.com/media/users/3/200117/isolated/preview/454ff5e4e43e4ee304b6400b4454704d-casa-simple-icono-casa-by-vexels.png" type="image/x-icon" />
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+<!-- JavaScript -->
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
+	</head>
+	<body style="background-image: url('https://th.bing.com/th/id/R.8fd6ffa811b46ea593fea1566d749c97?rik=x%2fRi%2fyzkEWLHbg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-2Gasb7hHsvQ%2fUTNynh9u52I%2fAAAAAAAAkIM%2fDhRSJLBTQQI%2fs1600%2fsala-grande-muy-moderna.jpg&ehk=Hivk94MkUui5IfIdWite2NHoHiuiqXUWzh2WqywFkjM%3d&risl=&pid=ImgRaw&r=0'); background-repeat: no-repeat; background-size: cover;">
+	
+<!-- Image and text -->
+<nav class="navbar  fixed-top  navbar-expand-lg navbar-dark" style="
+            background: hsla(0, 0%, 10%, 0.55);
+            backdrop-filter: blur(10px);
+            ">
+  <a class="navbar-brand" href="index.php">
+    <img src="https://images.vexels.com/media/users/3/200117/isolated/preview/454ff5e4e43e4ee304b6400b4454704d-casa-simple-icono-casa-by-vexels.png" height="30" class="d-inline-block align-top"
+      alt="mdb logo"> Mueblería
+  </a>
+  
+</nav>
+
+<!-- Section: Design Block -->
+<section class="text-center text-lg-start">
+  <style>
+    .cascading-right {
+      margin-right: -50px;
+    }
+
+    @media (max-width: 991.98px) {
+      .cascading-right {
+        margin-right: 0;
+      }
+    }
+  </style>
+  <hr>
+   <br>
+  <!-- Jumbotron -->
+  <div class="container py-2">
+    <div class="row g-0 align-items-center">
+      <div class="col-lg-6 mb-5 mb-lg-0">
+        <div class="card cascading-right" style="
+            background: hsla(0, 0%, 100%, 0.55);
+            backdrop-filter: blur(30px);
+            ">
+     
+          <div class="card-body p-5 shadow-5 text-center">
+            <h3 class="fw-bold mb-3">Inicia sesión</h2>
+         
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" id="form3Example3" class="form-control" name="correoElectronico" />
+                <label class="form-label" for="form3Example3">Email address</label>
+              </div>
+
+              <!-- Password input -->
+              <div class="form-outline mb-4">
+                <input type="password" id="form3Example4" class="form-control" name="contrasena"/>
+                <label class="form-label" for="form3Example4">Password</label>
+              </div>
+
+              <!-- Checkbox -->
+          
+
+              <!-- Submit button -->
+              <button type="submit" class="btn btn-primary btn-block mb-4"  name="login" value="login">
+              Iniciar Sesión
+              </button>
+              <label class="form-check-label" for="form2Example3">
+                
+                      <a href="recuperar.php"> Olvide contraseña</a>
+                      <br>
+                       <a href="registro.php">Registrame</a>
+                    </label>
+              <!-- Register buttons -->
+              
+            </form>
+            <!-- Button trigger modal -->
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 mb-5 mb-lg-0">
+        <img src="https://th.bing.com/th/id/OIP.NhGnKi5uxs-44Mz8jVVlPgDMEx?pid=ImgDet&rs=1" class="w-100 rounded-4 shadow-4"
+          alt="" />
+      </div>
+    </div>
+  </div>
+  <!-- Jumbotron -->
+</section>
+
+</body>
+
+</html>
+
